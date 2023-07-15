b089dba17d03b5cd6a995e8a6964568c6cec6373
Kauê Rodrigues <kauefr@gmail.com>
2023-07-14 21:50:23 -0300

    build 2023-07-14

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -6,5 +6,6 @@ export default function Dashboard(): JSX.Element {
     return <>
         <h1>kaue.rodrigues.nom.br</h1>
         <Link to="ssi">Seven Seas Idle</Link>
+        <p>2023-07-14</p>
     </>
 }
