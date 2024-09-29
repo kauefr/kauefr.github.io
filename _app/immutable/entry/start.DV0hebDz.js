f6b5f2e56359f3da32fc4824ec850c843747d44e
Kauê Rodrigues <kauefr@gmail.com>
2024-09-29 02:50:22 -0300

    Updates

---
+++ _app/immutable/entry/start.DV0hebDz.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CGZK15EW.js";export{t as start};
---
+++ _app/immutable/nodes/2.BqS5mrJa.js
@@ -0,0 +1,1 @@
+import{a as o,t as e}from"../chunks/disclose-version.DI8A8_75.js";var t=e("<p>Página pessoal de Kauê Rodrigues</p>");function n(a){var p=t();o(a,p)}export{n as component};
