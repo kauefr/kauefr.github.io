68f073293ac241f900398ee3a4b7b4b29ad9fa45
Kauê Rodrigues <kauefr@gmail.com>
2025-06-10 23:47:44 -0300

    PrimeNG e tailwind instalados, closes #10

---
+++ src/app/pages/angular-demo/angular-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-demo',
+  imports: [],
+  templateUrl: './angular-demo.component.html',
+  styleUrl: './angular-demo.component.scss'
+})
+export class AngularDemoComponent {
+  title = 'Angular Demo';
+}
