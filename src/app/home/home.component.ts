747580951e9b2e09900f8fd220dfa190412fe27c
Kauê Rodrigues <kauefr@gmail.com>
2025-08-15 19:29:12 -0300

    module import -> component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { PanelModule } from 'primeng/panel';
+import { Panel } from 'primeng/panel';
 import { Fieldset } from 'primeng/fieldset';
 import { NavbarComponent } from '../core/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [PanelModule, Fieldset, NavbarComponent],
+  imports: [Panel, Fieldset, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
