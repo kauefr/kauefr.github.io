4859c4d3f521d743b9566616c5d7a0cc14f784d4
Kauê Rodrigues <kauefr@gmail.com>
2026-09-13 02:45:42 -0300

    ComparadorCdbLcaComponent

---
+++ apps/angular14/src/app/comparador-cdb-lca/comparador-cdb-lca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-comparador-cdb-lca',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './comparador-cdb-lca.component.html',
+})
+export class ComparadorCdbLcaComponent {}
