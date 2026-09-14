a3a1d8c6489f69f9a4e5aa0457bc4579f273e966
Kauê Rodrigues <kauefr@gmail.com>
2026-09-13 22:57:43 -0300

    correção baseURL no projeto TS

--- apps/ts/vite.config.ts
+++ apps/ts/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 const docs = resolve(import.meta.dirname, "../../docs");
 
 export default defineConfig({
+  base: "/apps/ts/",
   build: {
     outDir: resolve(docs, "apps/ts"),
     emptyOutDir: true,
